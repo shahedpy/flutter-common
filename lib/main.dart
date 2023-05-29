c61c92af66e89acd4e96a68453f4827b9889ab37
Shahed <shahed_hridoy@outlook.com>
2023-05-29 16:58:25 +0600

    connectivity test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,53 +17,94 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Common'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Internet Connectivity
+  late bool _isConnected;
+  late StreamSubscription<InternetConnectionStatus> _connectionSubscription;
+  bool _showStatus = false;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _checkConnectivity();
+    _connectionSubscription =
+        InternetConnectionChecker().onStatusChange.listen((event) {
+      setState(() {
+        _isConnected = event == InternetConnectionStatus.connected;
+      });
+      if (_isConnected) {
+        _showStatus = true;
+        Timer(const Duration(seconds: 3), () {
+          setState(() {
+            _showStatus = false;
+          });
+        });
+      } else {
+        _showStatus = true;
+      }
     });
+  }
+
+  @override
+  void dispose() {
+    _connectionSubscription.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      bottomNavigationBar: _showStatus
+          ? _isConnected
+              ? BottomAppBar(
+                  child: Container(
+                    height: 24,
+                    color: Colors.green,
+                    child: const Center(
+                      child: Text(
+                        "Back online",
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                )
+              : BottomAppBar(
+                  child: Container(
+                    height: 24,
+                    color: Colors.red,
+                    child: const Center(
+                      child: Text(
+                        "No connection",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+          : Container(),
     );
+  }
+
+  Future<void> _checkConnectivity() async {
+    final isConnected = await InternetConnectionChecker().hasConnection;
+    setState(() {
+      _isConnected = isConnected;
+    });
   }
 }
