408757c69911bf6bfed38d947e16dfa4d1ed7c08
Shahed <shahed_hridoy@outlook.com>
2023-07-30 17:08:59 +0600

    getx maintain

---
+++ lib/view/lottie_animation/component/lottie_animation_section.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:common/view_model/lottie_animation_view_model.dart';
+
+class LottieAnimationSection extends GetWidget<LottieAnimationViewModel> {
+  const LottieAnimationSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/view/lottie_animation/lottie_animation_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:common/view/lottie_animation/component/lottie_animation_section.dart';
+
+class LottieAnimationView extends StatelessWidget {
+  const LottieAnimationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LottieAnimationSection();
+  }
+}
