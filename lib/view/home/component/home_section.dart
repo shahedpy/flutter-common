334ec0d7355844a66b91cd0c276d541c29a112b9
Shahed <shahed_hridoy@outlook.com>
2023-07-30 17:13:10 +0600

    get

---
+++ lib/view/home/component/home_section.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:common/view_model/home_view_model.dart';
+
+class HomeSection extends GetWidget<HomeViewModel> {
+  const HomeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
