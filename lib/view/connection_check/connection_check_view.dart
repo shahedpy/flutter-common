a3834f491fec22f914317c047d2221f821b7186f
Shahed <shahed_hridoy@outlook.com>
2023-07-30 17:46:36 +0600

    connection

---
+++ lib/view/connection_check/connection_check_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:common/view/connection_check/component/connection_check_section.dart';
+
+class ConnectionCheckView extends StatelessWidget {
+  const ConnectionCheckView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ConnectionCheckSection();
+  }
+}
