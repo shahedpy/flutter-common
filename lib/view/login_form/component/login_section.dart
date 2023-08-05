a8e0c918f1f4f15429b482af24c4da9cb12c8307
Shahed <shahed_hridoy@outlook.com>
2023-08-05 12:11:55 +0600

    login validation

--- lib/view/login_form/component/login_section.dart
+++ lib/view/login_form/component/login_section.dart
@@ -1,12 +1,49 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import 'package:common/view_model/login_view_model.dart';
+import '../../../view_model/login_view_model.dart';
 
 class LoginSection extends GetWidget<LoginViewModel> {
-  const LoginSection({super.key});
+  const LoginSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: TextField(
+                  onChanged: (value) => controller.username.value = value,
+                  decoration: const InputDecoration(
+                    hintText: "Username",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: TextField(
+                  onChanged: (value) => controller.password.value = value,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Password",
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => controller.validateLogin(),
+                child: const Text("Login"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/view_model/login_view_model.dart
+++ lib/view_model/login_view_model.dart
@@ -1,3 +1,18 @@
 import 'package:get/get.dart';
+import 'package:flutter/foundation.dart';
 
-class LoginViewModel extends GetxController {}
+class LoginViewModel extends GetxController {
+  RxString username = "".obs;
+  RxString password = "".obs;
+
+  void validateLogin() {
+    if (username.value.isEmpty || password.value.isEmpty) {
+      Get.snackbar('Error', 'Please enter both username and password');
+    } else {
+      if (kDebugMode) {
+        print('Username: ${username.value}');
+        print('Password: ${password.value}');
+      }
+    }
+  }
+}
